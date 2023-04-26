6503af42475b8263d37cd0634b9c22dccad543d1
danielalvarez2000 <122405591+danielalvarez2000@users.noreply.github.com>
2023-04-26 16:48:12 -0700

    yes

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ const matrixGenerator = (cardValues, size = 4) => {
                         winCount += 1;
                         //check if winCount ==half of cardValues
                         if (winCount == Math.floor(cardValues.length / 2)) {
-                            result.innerHTML = `<h2>You Won</h2>
+                            result.innerHTML = `<h2>You Won 8 5 1 2</h2>
             <h4>Moves: ${movesCount}</h4>`;
                             stopGame();
                         }
